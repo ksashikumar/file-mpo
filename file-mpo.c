515b2adae926ff74267aba98c5a3e63e2927478c
ksashikumar <ksashikumark93@gmail.com>
2013-04-25 00:07:39 +0530

    Resolve segfaults

--- file-mpo.c
+++ file-mpo.c
@@ -2,6 +2,7 @@
 
 #include <errno.h>
 #include <string.h>
+#include <stdlib.h>
 
 #include <fcntl.h>
 #include <sys/stat.h>
@@ -18,8 +19,6 @@
 #endif
 
 #include "libgimp/gimp.h"
-#include "libgimp/gimpui.h"
-
 #include "libgimp/stdplugins-intl.h"
 
 
@@ -27,7 +26,6 @@
 #define SAVE_PROC      "file-mpo-save"
 #define PLUG_IN_BINARY "file-mpo"
 #define PLUG_IN_ROLE   "gimp-file-mpo"
-#define PREVIEW_SIZE   350
 
 static void              query               (void);
 static void              run                 (const gchar      *name,
@@ -38,24 +36,15 @@ static void              run                 (const gchar      *name,
 
 
 static size_t            get_file_size       (const gchar  *filename);
-static gint32            load_image          (const gchar  *filename,
+static gboolean          load_image          (const gchar  *filename,
                                               GError      **error);
 static gboolean          split_mpo           (const gchar  *filename);
-/*static GimpPDBStatusType save_image          (const gchar      *filename,
-                                              gint32            image_id,
-                                              gint32            drawable_id,
-                                              GError          **error); 
-static gboolean          save_dialog         (const gchar      *filename,
-                                              gint32            image_id,
-                                              gint32            drawable_id); */
 
 
-
-
-static gint           file_ref = -1;
 static gint           num_images = 0; /*Number of images in MPO file*/
-static gchar  **image_name;
+static gchar        **image_name;
 static FILE          *fp;
+static gint32         image_id;
 
 const GimpPlugInInfo PLUG_IN_INFO =
 {
@@ -82,15 +71,6 @@ query (void)
     { GIMP_PDB_IMAGE, "image", "Output image" }
   };
 
-/*  static const GimpParamDef save_args[] =
-  {
-    { GIMP_PDB_INT32,    "run-mode",     "The run mode { RUN-INTERACTIVE (0) }" },
-    { GIMP_PDB_IMAGE,    "image",        "Input image"                  },
-    { GIMP_PDB_DRAWABLE, "drawable",     "Drawable to save"             },
-    { GIMP_PDB_STRING,   "filename",     "The name of the file to save the image in" },
-    { GIMP_PDB_STRING,   "raw-filename", "The name entered"             }
-  };
-*/
   gimp_install_procedure (LOAD_PROC,
                           "Load MPO files",
                           "No help till now",
@@ -106,20 +86,7 @@ query (void)
 
   gimp_register_load_handler (LOAD_PROC, "mpo", "");
 
-/*  gimp_install_procedure (SAVE_PROC,
-                          "Save images in MPO format",
-                          "No help till now",
-                          "Sashi Kumar <ksashikumark93@gmail.com>",
-                          "Sashi Kumar <ksashikumark93@gmail.com>",
-                          "24th April 2013",
-                          N_("3D MPO Data"),
-                          "INDEXED, GRAY, RGB, RGBA",
-                          GIMP_PLUGIN,
-                          G_N_ELEMENTS (save_args), 0,
-                          save_args, NULL);
 
-  gimp_register_save_handler (SAVE_PROC, "", "");
-*/
 }
 
 static void
@@ -133,8 +100,6 @@ run (const gchar      *name,
   GimpRunMode        run_mode;
   GimpPDBStatusType  status = GIMP_PDB_SUCCESS;
   GError            *error  = NULL;
-  gint32             image_id;
-  gint32             drawable_id;
 
   INIT_I18N ();
 
@@ -148,60 +113,29 @@ run (const gchar      *name,
 
   if (strcmp (name, LOAD_PROC) == 0)
     {
-      if (run_mode == GIMP_RUN_INTERACTIVE)
+      
+      if (load_image (param[1].data.d_string, &error))
         {
-          file_ref = g_open (param[1].data.d_string, O_RDONLY, 0);
-
-          if (file_ref < 0)
-            {
-              g_set_error (&error,
-                           G_FILE_ERROR, g_file_error_from_errno (errno),
-                           _("Could not open '%s' for reading: %s"),
-                           gimp_filename_to_utf8 (param[1].data.d_string),
-                           g_strerror (errno));
-
-              status = GIMP_PDB_EXECUTION_ERROR;
-            }
-          else
-            {
-              if (! load_image (param[1].data.d_string, &error))
-                status = GIMP_PDB_CANCEL;
-
-              close (file_ref);
-            }
+          *nreturn_vals = 2;
+           values[1].type         = GIMP_PDB_IMAGE;
+           values[1].data.d_image = image_id;
         }
       else
-        { /* Need to change*/
-          status = GIMP_PDB_CALLING_ERROR;
-        }
-
-
-      if (status == GIMP_PDB_SUCCESS)
         {
-
-          if (load_image (param[1].data.d_string, &error))
-            {
-              *nreturn_vals = 2;
-              values[1].type         = GIMP_PDB_IMAGE;
-              values[1].data.d_image = image_id;
-            }
-          else
-            {
-              status = GIMP_PDB_EXECUTION_ERROR;
-            }
+          status = GIMP_PDB_EXECUTION_ERROR;
         }
     }
-/*  else if (strcmp (name, SAVE_PROC) == 0)
+  else
     {
-
+      status = GIMP_PDB_CALLING_ERROR;
     }
-*/
+
 
   if (status != GIMP_PDB_SUCCESS && error)
     {
       *nreturn_vals = 2;
-      values[1].type          = GIMP_PDB_STRING;
-      values[1].data.d_string = error->message;
+      values[1].type           = GIMP_PDB_STRING;
+      values[1].data.d_string  = error->message;
     }
 
   values[0].data.d_status = status;
@@ -225,7 +159,6 @@ static gboolean
 load_image (const gchar  *filename,
             GError      **error)
 {
-  gint32  image_id;
   gint32  layer_id;
   gint32  size;
   gint    i;
@@ -290,30 +223,22 @@ split_mpo (const gchar  *filename)
   gchar *temp;
 
   fnmbase = strdup(filename);
-  ext = strstr(fnmbase,".mpo");
+  ext = strstr(fnmbase,".MPO");
 
   if (ext != NULL) 
       ext[0] = '\0';
 
   /*obtain file size:*/
-  length = get_file_size (filename);
+  fseek(fp, 0, SEEK_END);
+  length = ftell(fp);
+  rewind(fp);
 
   /*allocate memory to contain the whole file:*/
   buffer = g_new0 (gchar ,length);
-  if (buffer == NULL)  
-    {
-      /*g_set_error (error, G_FILE_ERROR, g_file_error_from_errno (errno),
-                   _("failed to allocate memory"));*/
-      return FALSE;
-    } 
- 
+
   amount = fread(buffer,1,length,fp);
   if (amount != length) 
-    {
-      /*g_set_error (error, G_FILE_ERROR, g_file_error_from_errno (errno),
-                   _("error loading file"));*/
       return FALSE;
-    }
   fclose(fp);
 
   /*Now find the individual JPGs*/
@@ -335,14 +260,13 @@ split_mpo (const gchar  *filename)
                       num_images++;
                       if (last != NULL) 
                         {
-                          /*copy out the previous view*/
-                   
-                          image_name[i] = g_new (gchar, strlen(fnmbase)+30); /*Bad Assumption*/
-                          sprintf(temp, "%s.image#%d.jpg", fnmbase, num_images-1);
-                          image_name[i] = strdup (temp);
-                          FILE* w = fopen(image_name[i++], "wb");
+                          /*copy out the previous view*/                   
+                          image_name[i] = malloc (sizeof(gchar) * 30); /*Bad Assumption*/
+                          sprintf(image_name[i], "%s.image#%d.jpg", fnmbase, num_images-1);
+                          FILE* w = fopen(image_name[i], "wb");
                           fwrite(last, 1, view-last, w);
                           fclose(w);
+                          i++;
                         }
                       last = view;
                       view+=4;
@@ -363,36 +287,16 @@ split_mpo (const gchar  *filename)
   if (num_images > 1) 
     {
       
-      image_name[i] = g_new (gchar, strlen(fnmbase)+30);
-      sprintf(temp, "%s.image#%d.jpg", fnmbase, num_images);
-      image_name[i] = strdup (temp);
+      image_name[i] = malloc (sizeof(gchar) * 30);
+      sprintf(image_name[i], "%s.image#%d.jpg", fnmbase, num_images);
       FILE* w = fopen(image_name[i], "wb");
       fwrite(last, 1, buffer+length-last, w);
       fclose(w);
     } 
-  /*if (num_images == 0)
-    g_set_error (error, G_FILE_ERROR, g_file_error_from_errno (errno),
-                 _("No views found.")); */
 
   g_free(buffer);
 
-}
-
-/*
-static GimpPDBStatusType
-save_image (const gchar  *filename,
-            gint32        image_id,
-            gint32        drawable_id,
-            GError      **error)
-{
+  return TRUE;
 
 }
 
-static gboolean
-save_dialog (const gchar *filename,
-             gint32       image_id,
-             gint32       drawable_id)
-{
-
-}
-*/
